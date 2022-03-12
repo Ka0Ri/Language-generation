6a2490b894a58c860be0215a739dd7d55342adb6
Kaori <dtvu1707@gmail.com>
2022-03-12 05:56:48 +0000

    fix

--- data-augmentation/src/augmentation.py
+++ data-augmentation/src/augmentation.py
@@ -1,5 +1,7 @@
+from cgitb import text
 from http.client import OK
 from operator import imod
+from tkinter.messagebox import NO
 import numpy as np
 import os
 import random
@@ -11,6 +13,8 @@ from koeda.augmenters.insertion import RandomInsertion
 from koeda.augmenters.swap import RandomSwap
 from koeda.augmenters.replacement import SynonymReplacement
 from konlpy.tag import Okt
+from googletrans import Translator
+from easynmt import EasyNMT
 
 logger = logging.getLogger(__name__)
 
@@ -77,14 +81,74 @@ class DataAugmentaion(object):
             for line in sub_lines:
                 f.write(line)
                 f.write('\n')
+    
+    @classmethod
+    def augment_data(self, src_path, tgr_path, data_type, mode="eda", p=0.2, k=1):
+        # translator_en = Translator()
+        translator = EasyNMT('mbart50_m2m')
+        examples = self.process_data(src_path, data_type)
+        aug_examples = []
+        for example in examples:
+            guid = example.guid
+            example.guid = guid + "-org"
+            text_a = example.text_a
+            text_b = example.text_b
+            if(text_b != None):
+                inputs = [text_a, text_b]
+            else:
+                inputs = [text_a]
+            try:
+                if(mode == 'eda'):
+                    text_aug = self.aug_EAD(inputs, p, k=k)
+                elif(mode == "bt"):
+                    aug_list = []
+                    for sentence in inputs:
+                        eng_trans = translator.translate(sentence, source_lang="ko", target_lang='en')
+                        ko_src = translator.translate(eng_trans, source_lang="en", target_lang='ko')
+                        aug_list.append(ko_src)
+                    text_aug = aug_list
+                else:
+                    raise NotImplementedError
+                aug_examples.append(example) # add origial example
+                for ki in range(k):
+                    text_aug_ki = text_aug[ki]
+                    if(len(text_aug_ki) == 1):
+                        text_aug_ki.append(None)
+                    #extend with augmentation example
+                    aug_examples.append(InputExample(guid=guid + "-aug", text_a=text_aug_ki[0], text_b=text_aug_ki[1], label=example.label))
+            except:
+                continue
+        self.write_data(aug_examples, tgr_path)
+        return aug_examples
             
     @classmethod
-    def save_data(self, sub_lines, path):
+    def write_data(self, examples, path):
+        data_type = examples[0].guid.split("-")[0]
+        logger.info("wirte to %s"%path)
         with open(path, "w", encoding="utf-8") as f:
-            for line in sub_lines:
-                f.write(line)
-                f.write('\n')
-        pass
+            #write header
+            if(data_type == "nli"):
+                f.write('sentence1\tsentence2\tgold_label\n')
+            elif(data_type == "sts"):
+                f.write('genre\tfilename\tyear\tid\tscore\tsentence1\tsentence2\n')
+            elif(data_type == "nscm"):
+                f.write('id\tdocument\tlabel\n')
+            elif(data_type == "questionpair"):
+                f.write('question1\tquestion2\tis_duplicate\n')
+            else:
+                raise NotImplementedError
+            #write contents  
+            for example in examples:
+                if(data_type == "nli"):
+                    f.write('%s\t%s\t%s\n'%(example.text_a, example.text_b, example.label))
+                elif(data_type == "questionpair"):
+                    f.write('%s\t%s\t%s\n'%(example.text_a, example.text_b, example.label))
+                elif(data_type == "sts"):
+                    f.write('None\tNone\tNone\t%s\t%s\t%s\t%s\n'%(example.guid, example.label, example.text_a, example.text_b))
+                elif(data_type == "nscm"):
+                     f.write('%s\t%s\t%s\n'%(example.guid, example.text_a, example.label))
+                else:
+                    raise NotImplementedError
     
     @classmethod
     def process_data(self, path, set_type):
@@ -111,7 +175,7 @@ class DataAugmentaion(object):
                     logger.info(line)
                 examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
             return examples
-        elif(set_type in ["question-pair", 'nli']):
+        elif(set_type in ["questionpair", 'nli']):
             for (i, line) in enumerate(lines[1:]):
                 line = line.split('\t')
                 guid = "%s-%s" % (set_type, i)
@@ -122,10 +186,11 @@ class DataAugmentaion(object):
                     continue
                 if i % 100000 == 0:
                     logger.info(line)
+                
                 examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
             return examples
         else:
-            raise NotImplementedError()
+            raise NotImplementedError
 
     @classmethod
     def aug_synonym_replacement(self, inputs, p=0.1, k=2):
@@ -162,20 +227,46 @@ class DataAugmentaion(object):
             sr_aug = aug.random_deletion(sentence, p = p, repetition=k)
             aug_list.append(sr_aug)
         return aug_list
-        
+    
+    @classmethod
+    def aug_BT_EasyNMT(self, inputs, k=2):
+        translator = EasyNMT('mbart50_m2m')
+        aug_list = []
+        for sentence in inputs:
+            ko_src = sentence
+            for i in range(k):
+                eng_trans = translator.translate(ko_src, source_lang="en", target_lang='ko')
+                ko_src = translator.translate(eng_trans, source_lang="ko", target_lang='en')
+                aug_list.append(ko_src)
+        return aug_list
+
+    @classmethod
+    def aug_BT_googleAPI(self, inputs, k=2):
+        translator = Translator()
+        aug_list = []
+        for sentence in inputs:
+            ko_src = sentence
+            for i in range(k):
+                eng_trans = translator.translate(ko_src, src='ko', dest='en').text
+                ko_src = translator.translate(eng_trans, src='en', dest='ko').text
+                aug_list.append(ko_src)
+        return aug_list
 
     @classmethod
     def aug_EAD(self, inputs, p = 0.1, k=2):
-        aug = [RandomDeletion(Okt()),
+        aug = [
+                SynonymReplacement(Okt()),
                 RandomInsertion(Okt()),
                 RandomSwap(Okt()),
-                RandomDeletion(Okt())]
+                RandomDeletion(Okt())
+                ]
         aug_list = []
-        for sentence in inputs:
-            ridx = random.randint(0, 3)
-            for i in range(k):
+        for i in range(k):
+            tuple = []
+            for sentence in inputs:
+                ridx = random.randint(0, 1)
                 sr_aug = aug[ridx](sentence, p = p, repetition=1)
-                aug_list.append(sr_aug)
+                tuple.append(sr_aug)
+            aug_list.append(tuple)
         return aug_list
-    
     
